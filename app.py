23bd43c000c257ddc0835fbed3ba0d2ae9c92446
Kshitij Mishra <kshitijm077@gmail.com>
2026-07-01 15:47:59 +0530

    Update title from 'Movie Recommender System' to 'Movie Recommender'

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def recommend(movie):
 
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
-st.title("Movie Recommender System")
+st.title("Movie Recommender")
 selected_movie_name = st.selectbox('Select a movie to get similar recommendations', movies['title'].values)
 
 
